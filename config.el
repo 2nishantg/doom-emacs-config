7ad304db905b0dcce7ba7e90b62e4d35f18bcbf3
Nishant Gupta <nishant.gupta@rubrik.com>
2020-01-09 00:01:49 -0800

    Add initial config files

---
+++ config.el
@@ -0,0 +1,74 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Nishant Gupta"
+      user-mail-address "nishant.gupta@rubrik.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Iosevka" :size 16))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-one)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type nil)
+(setq-default evil-escape-key-sequence "fd")
+
+(map! :leader
+      :desc "Switch to Buffer"       "TAB"    #'evil-switch-to-windows-last-buffer
+      :desc "Vertical split"         "w2"     #'evil-window-vsplit
+      :desc "Switch to right"        "2"      #'evil-window-right
+      :desc "Switch to left"         "1"      #'evil-window-left
+      :desc "Horizontal split"       "w-"      #'evil-window-split
+)
+(define-key global-map (kbd "C-x C-c") 'ido-kill-buffer)
+(global-set-key (kbd "M-n") 'forward-paragraph)
+(global-set-key (kbd "M-p") 'backward-paragraph)
+(global-set-key (kbd "C-d") 'delete-forward-char)
+(global-set-key (kbd "C-h")  'delete-backward-char)
+(global-set-key (kbd "M-y")  'counsel-yank-pop)
+
+(setq column-number-mode t)
+(fringe-mode 0)
+
+(defadvice kill-region (before unix-werase activate compile)
+  "When called interactively with no active region, delete a single word
+    backwards instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (save-excursion (backward-word 1) (point)) (point)))))
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
